194fea80f7a3e1031acf35ba8515ab215e908edf
unknown <chinchanilavanya@gmail.com>
2023-04-09 20:48:17 +0530

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,70 @@ const Header =(props)=>{
             <Content>
                 <Logo>
                     <a href="/home">
-                        <img src="/images/home-logo.svg" alt=""/>
+                    <img src="/images/home-logo .svg" alt=""/>
                     </a>
                 </Logo>
                 <Search>
                     <div>
-                        <input type="text" plceholder="Search"/>
+                    <input type="text" placeholder="Search"/>
                     </div>
                     <SearchIcon>
                     <img src="/images/search-icon.svg" alt="" />
                     </SearchIcon>
                 </Search>
+                <Nav>
+                <NavListWrap>
+                    <NavList className="active">
+                        <a>
+                            <img src="/images/nav-home.svg" alt="" />
+                            <span>Home</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-network.svg" alt="" />
+                            <span>My Network</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-jobs.svg" alt="" />
+                            <span>Job</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-messaging.svg" alt="" />
+                            <span>Messaging</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-notifications.svg" alt="" />
+                            <span>Notifications</span>
+                        </a>
+                    </NavList>
+                    <User>
+                        <a>
+                        <img src="/images/user.svg" alt=""/>
+                        <span>Me</span>
+                        <img src="/images/down-icon.svg" alt=""/>
+                        </a>
+                        <SignOut>
+                            <a>
+                            SignOut   
+                            </a>
+                        </SignOut>
+                    </User>
+                    <Work>
+                        <img src="/images/nav-work.svg" alt=""/>
+                        <span>
+                            Work
+                            <img src="/images/down-icon.svg" alt=""/>
+                        </span>
+                    </Work>
+                </NavListWrap>
+                </Nav>
             </Content>
         </Container>
     );
@@ -24,7 +77,7 @@ const Header =(props)=>{
 
 const Container =styled.div`
 background-color:white;
-border-bottom:1px solid rgba(0,0,0,0,08);
+border-bottom:1px solid rgba(0,0,0,0,0.8);
 left:0;
 padding :0 2px;
 position: fixed;
@@ -48,7 +101,7 @@ opacity:1;
 flex-grow:1;
 position:relative;
 & > div {
-    max-width:none;
+    max-width:280px;
     input{
         border:none;
         box-shadow:none;
@@ -79,4 +132,115 @@ display:flex;
 justify-content:center;
 align-items:center;
 `;
+const Nav =styled.nav`
+margin-left:auto;
+display:block;
+@media (max-width:768px)
+{
+    position:fixed;
+    left:0;
+    bottom:0;
+    background:white;
+    width:100%;
+}
+`;
+const NavListWrap=styled.ul`
+display:flex;
+flex-wrap:nowrap;
+list-style-type: none;
+
+    .active {
+    span:after {
+      content: "";
+      transform: scaleX(1);
+      border-bottom: 2px solid var(--white, #fff);
+      bottom: 0;
+      left: 0;
+      position: absolute;
+      transition: transform 0.2s ease-in-out;
+      width: 100%;
+      border-color: rgba(0, 0, 0, 0.9);
+    }
+}
+
+`;
+
+const NavList =styled.li`
+display:flex;
+align-items:center;
+a{
+    align-items:center;
+    background:transparent;
+    display:flex;
+    flex-direction:column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    min-height: 52px;
+    min-width: 80px;
+    position: relative;
+    text-decoration: none;
+
+    span {
+      color: rgba(0, 0, 0, 0.6);
+      display: flex;
+      align-items: center;
+    }
+    @media (max-width: 768px) {
+      min-width: 70px;
+    }
+}
+&:hover,
+  &:active {
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
+`;
+const SignOut=styled.div`
+position:absolute;
+top:45px;
+background:white;
+border-radius:0 0 5px 5px;
+width:100px;
+height:40px;
+font-size:16px;
+transition-duration:167px;
+text-align:center;
+display:none;
+`;
+
+const User = styled(NavList)`
+a > svg 
+{
+    width:24px;
+    border-radius:50%;
+
+}
+a >img {
+    width:24px;
+    height:24px;
+    border-radius:50%;
+}
+span {
+    display:flex;
+    align-items:center;
+}
+
+    &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+}
+`;
+const Work = styled(User)`
+border-left:1px solid rgba(0,0,0,0.08);
+`;
+
+
 export default Header;
